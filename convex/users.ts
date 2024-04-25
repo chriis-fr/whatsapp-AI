e7bd3db4207399aeca6b30b325e5aaf8af556d96
christopher odhiambo <caspianodhis@gmail.com>
2024-04-26 00:44:30 +0300

    before sleeping

--- convex/users.ts
+++ convex/users.ts
@@ -19,6 +19,27 @@ export const createUser = internalMutation({
     }
 });
 
+export const updateUser = internalMutation({
+    args: {
+        tokenIdentifier: v.string(), image: v.string()
+    },
+    async handler(ctx, args) {
+        const user = await ctx.db
+        .query("users")
+        .withIndex("by_tokenIdentifier", (q) => q.eq("tokenIdentifier", args.tokenIdentifier))
+        .unique()
+
+        if(!user) {
+            throw new ConvexError("no user with this token found")
+        }
+
+        await ctx.db.patch(user._id, {
+            image: args.image,
+        })
+
+    }
+})
+
 export const setUserOffline = internalMutation({
     args: {tokenIdentifier: v.string()}, 
     handler: async (ctx, args) => {
@@ -34,4 +55,30 @@ export const setUserOffline = internalMutation({
     }
 })
 
+export const setUserOnline = internalMutation({
+    args: {tokenIdentifier: v.string()}, 
+    handler: async (ctx, args) => {
+        const user = await ctx.db
+        .query("users")
+        .withIndex("by_tokenIdentifier", (q) => q.eq("tokenIdentifier", args.tokenIdentifier))
+        .unique();
 
+        if (!user) {
+            throw new ConvexError("no user with this token found");
+        }
+        await ctx.db.patch(user._id, {isOnline: true})
+    }
+})
+
+export const getUsers = query({
+    args: {},
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if(!identity) {
+            throw new ConvexError("Unauthorized")
+        }
+
+        const users = await ctx.db.query("users").collect()
+        return users
+    },
+})
