feda54b24e0c665ecaa674ac515fad0106e8bdd0
christopher odhiambo <caspianodhis@gmail.com>
2024-04-27 00:38:16 +0300

    late night day 3

--- convex/users.ts
+++ convex/users.ts
@@ -79,7 +79,7 @@ export const getUsers = query({
         }
 
         const users = await ctx.db.query("users").collect()
-        return users
+        return users.filter((user) => user.tokenIdentifier !== identity.tokenIdentifier)
     },
 })
 
